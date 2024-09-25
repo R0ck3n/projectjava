2d25d4046ca21342423837e1e205a9b97f477f6f
Rocken <r0ck3n@hotmail.com>
2024-09-25 17:32:06 +0200

    page d'acceuil qui s'affiche

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,28 +6,59 @@ import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.InputStream;
 import java.net.InetSocketAddress;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        // Créer un serveur HTTP écoutant sur le port 8080
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
-        // Créer un point d'entrée /hello
-        server.createContext("/", new HelloHandler());
+        // Créer un gestionnaire pour les fichiers statiques (HTML, CSS, JS)
+        server.createContext("/", new StaticFileHandler());
 
-        // Démarrer le serveur
+        // Créer un gestionnaire pour /hello
+        server.createContext("/hello", new HelloHandler());
+
         server.setExecutor(null); // utilise le thread par défaut
         server.start();
         System.out.println("Serveur démarré sur le port 8080...");
+    }
+
+    // Gestionnaire pour servir les fichiers statiques
+    static class StaticFileHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String filePath = exchange.getRequestURI().getPath();
+            if (filePath.equals("/")) {
+                filePath = "/index.html"; // Page d'accueil par défaut
+            }
+
+            // Charger les fichiers depuis le classpath (dossier resources)
+            InputStream fileStream = getClass().getResourceAsStream("/static" + filePath);
+
+            if (fileStream != null) {
+                byte[] fileContent = fileStream.readAllBytes();
+                exchange.sendResponseHeaders(200, fileContent.length);
+
+                OutputStream os = exchange.getResponseBody();
+                os.write(fileContent);
+                os.close();
+            } else {
+                String response = "404 (Not Found)\n";
+                exchange.sendResponseHeaders(404, response.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        }
     }
 
     // Gestionnaire pour répondre aux requêtes HTTP sur /hello
     static class HelloHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            String response = "Hello, World!";
+            String response = "Hello, from Java!";
             exchange.sendResponseHeaders(200, response.length());
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
@@ -35,4 +66,3 @@ public class Main {
         }
     }
 }
-
