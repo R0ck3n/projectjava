4305107f671fff097a7514a8c64c8d59c86c9e36
Nicolas Gracia <r0kk3nn@gmail.com>
2024-10-09 20:52:13 +0200

    push

---
+++ target/classes/static/views/page2/js/script.js
@@ -0,0 +1,12 @@
+function fetchMessage() {
+    fetch("/hello")
+        .then((response) => response.text())
+        .then((message) => {
+            document.getElementById("message").textContent = message;
+        });
+}
+
+const btn = document.querySelector("#elButton");
+btn.addEventListener("click", () => {
+    fetchMessage();
+});
